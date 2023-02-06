49e1557e7cdae32f407427eff0177e9e4a1903f9
Jannah740 <rementerprise@gmail.com>
2023-02-06 14:11:00 +0200

    front end

---
+++ VUE/etimesheet/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <header>
+    <b-navbar toggleable="md">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand to="/">eTimesheet</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/time-sheet">Timesheet</b-nav-item>
+          <b-nav-item href="#" @click.prevent="login" v-if="!activeUser">Login</b-nav-item>
+          <b-nav-item href="#" @click.prevent="logout" v-else>Logout</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </header>
+    <!-- routes will be rendered here -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+}
+</script>
+<style>
+header {
+  background-color: #29C69F; ;
+}
+</style>
---
+++ VUE/etimesheet/src/components/Hello.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <div>
+  <b-alert show variant="secondary">Day</b-alert>
+  <b-alert show variant="success">Week</b-alert>
+  <b-alert show variant="danger">Month</b-alert>
+  <b-alert show variant="warning">Year</b-alert>
+  <b-alert show variant="info">Projects</b-alert>
+</div>
+  <b-calendar block locale="en-US"></b-calendar>
+</main>
+</template>
